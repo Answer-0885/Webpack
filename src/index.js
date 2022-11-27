efecea86376974c97b58fa83ce360a871778a10b
Vitaly <answer-0885@mail.ru>
2022-11-27 17:19:53 +0700

    Сделал форму

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import buttonBlock from './modules/buttonBlock';
 import form from './modules/form';
 import accordeon from './modules/accordeon';
+import burger from './modules/burger'; 
+import menu from './modules/menu'; 
 
 
 buttonBlock();
 form();
 accordeon();
+burger();
+menu();
